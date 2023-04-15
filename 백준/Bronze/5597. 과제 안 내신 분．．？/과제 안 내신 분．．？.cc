3df3ae7ca9eb28ff862c67778535d2209b2ffbec
Hyewon Yang <w2106@e-mirim.hs.kr>
2023-04-16 00:24:36 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -1,19 +1,16 @@
 #include <stdio.h>
-
-int main(void)
-{
-    int student[31] = { 0 };
-    int presentNum;
-
+int main(void) {
+    int arr[31] = { 0 };
+    int j;
     for (int i = 0; i < 28; i++) {
-        scanf("%d", &presentNum);
-        student[presentNum] = presentNum;
+        scanf("%d", &j);
+        arr[j] = j;
     }
-    for (int i = 1; i < 31; i++) {  
-        if (student[i] == 0)        
+    for (int i = 1; i < 31; i++) {
+        if (arr[i] == 0) {
             printf("%d\n", i);
+        }
     }
-
-
+    
     return 0;
 }
