2f5aa6de5e71b0f039b7930cf8a34488c654d261
Hyewon Yang <w2106@e-mirim.hs.kr>
2023-04-28 19:56:49 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+    int n, sum =0;
+    scanf("%d", &n);
+    char str[n];
+    scanf("%s", &str);
+    for(int i = 0; i<n; i++){
+        sum += str[i] - '0';
+    }
+    printf("%d", sum);
+    return 0;
+}
