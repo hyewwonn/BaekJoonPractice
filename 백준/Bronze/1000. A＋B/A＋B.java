8ac92549b47a8eda96e0233419a5693cfc161474
Hyewon Yang <w2106@e-mirim.hs.kr>
2023-05-11 23:51:49 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,10 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
+//bufferedReader 사용하여 입력받기
+//
 class Main{
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int A = scan.nextInt();
-        int B = scan.nextInt();
-        System.out.print(A+B);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //한 줄 읽어오기 readLine()
+        String str = br.readLine();
+        
+        //StringTokenizer( "문자열" , 구분자 );
+		StringTokenizer st = new StringTokenizer(str," ");
+        
+        //Integer.parseInt()로 int 형변환
+        //nextToken()으로 저장된 변수 차례대로 꺼내옴 (반환되면서 객체에서 사라짐)
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a+b);
     }
 }
