e27c82ed41c3399598d24e2b78f116112f196e7a
Hyewon Yang <w2106@e-mirim.hs.kr>
2023-11-20 17:52:41 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.js
@@ -2,8 +2,8 @@ function solution(numer1, denom1, numer2, denom2) {
     var answer = [];
     var numer = denom1*numer2 + denom2*numer1;
     var denom = denom1 * denom2;
-    var max = Math.max(numer, denom);
-    for(let i = max; i >=0; i--) {
+    var min = Math.min(numer, denom);
+    for(let i = min; i >=0; i--) {
         if(numer%i == 0 && denom%i == 0) {
             numer = numer/i;
             denom = denom/i;
