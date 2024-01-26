ace3014ff0816c85b7087d6a6bb4044164c689e0
Hyewon Yang <hyewwonn@gmail.com>
2024-01-26 14:32:31 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,8 +1,6 @@
 function solution(n) {
-    var i, j;
-    for (i = 0; !(n & 1); i++) {n = n >> 1; }
-    for (j = 0; n & 1; i++, j++) {n = n >> 1; }
-    for (j--, n++; i !== j; i--) {n = n << 1; }
-    for (i; i; i--, n++) {n = n << 1; }
-    return n;
+    var size = n.toString(2).match(/1/g).length
+    while(n++) {
+        if(size === n.toString(2).match(/1/g).length) return n;
+    }
 }
