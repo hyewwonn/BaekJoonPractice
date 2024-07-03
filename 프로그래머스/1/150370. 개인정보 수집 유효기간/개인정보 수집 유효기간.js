9cc1e2e95ebf7086e5a6f605047a7fa504dc1dfe
Hyewon Yang <hyewwonn@gmail.com>
2024-07-03 14:38:20 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.67 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,24 +1,30 @@
 function solution(today, terms, privacies) {
     const answer = [];
-
+    
     const [todayYear, todayMonth, todayDay] = today.split('.').map(Number);
     const todayDate = new Date(todayYear, todayMonth - 1, todayDay);
 
     const termMap = {};
     terms.forEach(term => {
         const [type, duration] = term.split(' ');
-        termMap[type] = parseInt(duration);
+        termMap[type] = parseInt(duration) * 28;
     });
+
+    const dateToDays = (year, month, day) => {
+        return year * 12 * 28 + month * 28 + day;
+    };
+
+    const todayDays = dateToDays(todayYear, todayMonth, todayDay);
 
     privacies.forEach((privacy, index) => {
         const [date, termType] = privacy.split(' ');
         const [year, month, day] = date.split('.').map(Number);
-        const collectedDate = new Date(year, month - 1, day);
-        const durationMonths = termMap[termType];
+        const collectedDays = dateToDays(year, month, day);
+        const expirationDays = termMap[termType];
 
-        const expiryDate = new Date(year, month - 1 + durationMonths, day);
+        const expiryDays = collectedDays + expirationDays;
 
-        if (expiryDate <= todayDate) {
+        if (expiryDays <= todayDays) {
             answer.push(index + 1);
         }
     });
