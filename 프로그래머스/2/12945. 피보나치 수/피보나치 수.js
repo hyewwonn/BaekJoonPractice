cbe4f3b3d974c9a9cfe9a49fe1ad508dfdbeb787
Hyewon Yang <hyewwonn@gmail.com>
2024-07-08 18:24:13 +0900

    [level 2] Title: 피보나치 수, Time: 31.53 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -2,8 +2,9 @@ function solution(n) {
     var answer = 0;
     var arr = [0,1];
     for(let i = 2; i<= n; i++) {
-        arr[i] = arr[i-2] + arr[i-1] % 1234567;
+        arr[i] = (arr[i-2] + arr[i-1]) % 1234567;
     }
     answer = arr[n] % 1234567;
+    console.log(answer, arr[n]);
     return answer;
 }
