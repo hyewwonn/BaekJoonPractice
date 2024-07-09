2290ed4227a3ec38bd282080e497f0d5b49f05dd
Hyewon Yang <hyewwonn@gmail.com>
2024-07-09 16:43:55 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 37.42 ms, Memory: 149 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    var answer = new Array(numbers.length).fill(-1);
+    var stack = [];
+
+    for(let i = 0; i < numbers.length; i++) {
+        while(stack.length > 0 && numbers[stack[stack.length - 1]] < numbers[i]) {
+            answer[stack.pop()] = numbers[i];
+        }
+        stack.push(i);
+    }
+
+    return answer;
+}
