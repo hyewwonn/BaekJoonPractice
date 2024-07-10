cbe64bea7b4fb5833bc521fc64f5afc92ceec823
Hyewon Yang <hyewwonn@gmail.com>
2024-07-10 11:11:35 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,14 +1,15 @@
 function solution(new_id) {
     var answer = new_id;
-    answer = answer.toLowerCase();
-    answer = answer.replace(/[^a-z0-9-_.]/g, '');
-    answer = answer.replace(/\.{2,}/g, '.').replace(/^\.|\.$/g, '');
-    if (answer === '') {
+    answer = answer.toLowerCase() // 1
+        .replace(/[^a-z0-9-_.]/g, '') // 2
+        .replace(/\.{2,}/g, '.') // 3
+        .replace(/^\.|\.$/g, ''); // 4
+    if (answer === '') { // 5
         answer = 'a';
     }
-    if (answer.length >= 16) {
+    if (answer.length >= 16) { // 6
         answer = answer.slice(0, 15).replace(/\.$/, '');
     }
-    answer = answer.padEnd(3, answer[answer.length-1]);
+    answer = answer.padEnd(3, answer[answer.length-1]); // 7
     return answer;
 }
