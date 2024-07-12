a3aa6036f60c844518a8299fa9c6f0ac7b575f43
Hyewon Yang <hyewwonn@gmail.com>
2024-07-12 11:37:47 +0900

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,7 +1,9 @@
 function solution(cards1, cards2, goal) {
+    let idx1 = 0;
+    let idx2 = 0;
     for(let i = 0; i< goal.length; i++) {
-        if(goal[i] === cards1[0]) cards1 = cards1.slice(1, cards1.length+1);
-        else if(goal[i] === cards2[0]) cards2 = cards2.slice(1, cards2.length+1);
+        if(goal[i] === cards1[idx1]) idx1++;
+        else if(goal[i] === cards2[idx2]) idx2++;
         else return 'No';
     }
     return 'Yes';
