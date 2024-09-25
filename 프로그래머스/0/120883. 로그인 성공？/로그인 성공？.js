dae780d41afa8f2ba0574503534534e3fbe8d178
Hyewon Yang <hyewwonn@gmail.com>
2024-09-25 15:28:20 +0900

    [level 0] Title: 로그인 성공?, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,9 @@
+function solution(id_pw, db) {
+    var answer = '';
+    const idx = db.findIndex((val) => val[0] === id_pw[0]);
+    if(idx !== -1) {
+        if(db[idx][1] === id_pw[1]) return 'login';
+        else return 'wrong pw';
+    }
+    else return 'fail';
+}
