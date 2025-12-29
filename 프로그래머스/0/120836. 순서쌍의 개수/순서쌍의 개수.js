c1581fbcaa953051bb5deb6a1920817e14025fa5
Hyewon Yang <hyewwonn@gmail.com>
2025-12-29 18:51:08 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,7 +1,10 @@
 function solution(n) {
     let answer = 0;
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) answer++;
+    for (let i = 1; i * i <= n; i++) {
+        if (n % i === 0) {
+            if (i * i === n) answer += 1;
+            else answer += 2;
+        }
     }
     return answer;
 }
